40115824c0a4126b642374eaabad98aaa0830615
MRbie <1748741328@qq.com>
2017-11-27 21:43:40 +0800

    初始hadoop的mapreduce框架

---
+++ src/com/bie/mapreduce/lesson04/WordCountMapper.java
@@ -0,0 +1,27 @@
+package com.bie.mapreduce.lesson04;
+/** 
+* @author  Author:别先生 
+* @date Date:2017年11月27日 下午9:31:22 
+*
+* 1:导入的包汇总
+* 	1):hadoop-2.6.4\share\hadoop\common下的
+* 		hadoop-common-2.6.4.jar
+* 		及其lib下面的所有
+* 	2):hadoop-2.6.4\share\hadoop\hdfs
+* 		hadoop-hdfs-2.6.4.jar
+* 		及其lib下面的所有
+* 	3):hadoop-2.6.4\share\hadoop\mapreduce
+* 		除了:hadoop-mapreduce-examples-2.6.4.jar
+* 		及其lib下面的所有
+* 	4):hadoop-2.6.4\share\hadoop\yarn
+* 		除了server的包
+* 		及其lib下面的所有。
+* 2:Mapreduce是一个分布式运算程序的编程框架,是用户开发“基于hadoop的数据分析应用”的核心框架;
+* 	Mapreduce核心功能是将用户编写的业务逻辑代码和自带默认组件整合成一个完整的分布式运算程序,
+* 	并发运行在一个hadoop集群上;	
+* 
+*/
+public class WordCountMapper{
+
+	
+}
---
+++ src/com/bie/mapreduce/lesson04/WordCountReducer.java
@@ -0,0 +1,10 @@
+package com.bie.mapreduce.lesson04;
+/** 
+* @author  Author:别先生 
+* @date Date:2017年11月27日 下午9:31:41 
+*
+*
+*/
+public class WordCountReducer {
+
+}
